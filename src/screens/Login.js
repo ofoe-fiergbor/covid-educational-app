e2a9664294a82d9f474a121d3a2ab32a3974055f
mcbryan1 <git config --global user.email solomonmcbryan@gmail.comgit config --global user.email>
2020-08-11 19:37:32 +0000

    Accepted Login Change

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 const Login = ({navigation}) => {
     return (
@@ -12,20 +12,23 @@ const Login = ({navigation}) => {
     )
 }
 
-export default Login
+ 
+
+
+export default Login;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    btn:{
-        marginVertical:50,
-        backgroundColor:'skyblue',
-        width:100,
-        justifyContent:'center',
-        alignItems:'center',
-        height:40
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn: {
+    marginVertical: 50,
+    backgroundColor: "skyblue",
+    width: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+  },
+});
